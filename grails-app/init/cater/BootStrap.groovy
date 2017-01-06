ed5cdfe935ce3c4b4e958429cad7b3bc6adedff9
Søren Berg Glasius <soeren@glasius.dk>
2017-01-06 22:43:11 +0530

    Bootstrap data

--- grails-app/init/cater/BootStrap.groovy
+++ grails-app/init/cater/BootStrap.groovy
@@ -1,8 +1,13 @@
 package cater
 
+import workshop.Item
+
 class BootStrap {
 
     def init = { servletContext ->
+        ['Espresso': 100, 'Latte': 100, 'Cappuccino': 100, 'Water': 50, 'Tea': 100, 'Snacks': 150, 'Chips': 150, 'Notepad': 0].each { name, price ->
+            new Item(name: name, price: price).save(failOnError: true)
+        }
     }
     def destroy = {
     }
