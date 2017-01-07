eeea62671031900681ddc7ce34f47cf785631860
Søren Berg Glasius <soeren@glasius.dk>
2017-01-08 00:15:45 +0530

    TagLib test

---
+++ src/test/groovy/cater/ItemTagLibSpec.groovy
@@ -0,0 +1,27 @@
+package cater
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+import spock.lang.Unroll
+import workshop.Item
+
+/**
+ * See the API for {@link grails.test.mixin.web.GroovyPageUnitTestMixin} for usage instructions
+ */
+@TestFor(cater.ItemTagLib)
+class ItemTagLibSpec extends Specification {
+
+    @Unroll
+    void "Test taglib"() {
+        given:
+        def item = new Item(name: 'Name', outOfStock: outOfStock)
+
+        expect:
+        applyTemplate('<i:orderItem item="${item}"/>', [item: item]) == expectedText
+
+        where:
+        outOfStock || expectedText
+        false      || '<a href="/order/create?item.id=&amount=1">Order</a>'
+        true       || '<span class="badge">Out of Stock</span>'
+    }
+}
